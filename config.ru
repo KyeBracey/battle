02d12f45ea4729022f5826867e659e9881b2729b
Kynosaur <kye.bracey@gmail.com>
2017-05-23 19:19:10 +0100

    Implement functionality for players to type in their names and view them.  Change test to use have_content instead of have_field

--- config.ru
+++ config.ru
@@ -1,2 +1,2 @@
-require './app.rb'
+require './lib/battle.rb'
 run Battle
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,16 +1,12 @@
 require 'battle'
 
 feature "Testing infrastructure" do
-  scenario 'can run app and change page content' do
-    visit('/')
-    expect(page).to have_content('Testing infrastructure working!')
-  end
 
   scenario 'players fill in their names and see them on screen' do
     visit('/')
-    fill_in('Player 1', with: 'Kye')
-    fill_in('Player 2', with: 'Kavita')
-    expect(page).to have_field('Player 1', with: 'Kye')
-    expect(page).to have_field('Player 2', with: 'Kavita')
+    fill_in('player_1', with: 'Kye')
+    fill_in('player_2', with: 'Kavita')
+    click_button 'Submit'
+    expect(page).to have_content 'Kye -VS- Kavita'
   end
 end
